7cec06e7e75090ec7b8713c0c31f33f5c968fe83
darkroom2 <rkomorowski97@gmail.com>
2021-12-02 15:18:50 +0100

    wip: almost done, stats todo

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -4,12 +4,10 @@ from logging import info, DEBUG
 from multiprocessing import Pool
 from pathlib import Path
 
-from numpy import mean
 from numpy.random import default_rng
-from scipy.stats import t, sem
 
 from simulation.simulator import Simulator
-from simulation.utils import setup_logger, get_max_traffic
+from simulation.utils import setup_logger
 
 
 class Simulation:
@@ -31,11 +29,6 @@ class Simulation:
             return {}
 
     def run(self):
-
-        # TODO: Wykresy zależności że np. P_block nie zmienia sie liniowo ze
-        #  wzrostem serwerow itp. ze wynik jest niezalezny od roznych rozkladow
-        #  czasu obslugi
-
         mi_values = self.config.get('mi_values', [0.6])
         lam_values = self.config.get('lam_values', [1])
         server_counts = self.config.get('server_counts', [1])
@@ -44,16 +37,13 @@ class Simulation:
 
         info(f'Running simulator with k = {sim_repetitions} repetitions for '
              f'each combination of μ, λ and server count values')
-        combinations = product(mi_values, lam_values, server_counts)
-        self.results = [
-            self.simulate(combination) for combination in combinations
-        ]
+        with Pool() as pool:
+            combinations = product(mi_values, lam_values, server_counts)
+            self.results = pool.map(self.simulate, combinations)
 
-        info(f'Results: {self.results}')
         Path(self.results_path).write_text(dumps(self.results))
 
     def simulate(self, combination):
-        show_plots = self.config.get('show_plots', True)
         sim_repetitions = self.config.get('simulation_repetitions', 10)
         time_limit = self.config.get('time_limit', 10)
         events_limit = self.config.get('events_limit', 10000)
@@ -78,19 +68,18 @@ class Simulation:
             sim.run()
 
             sim_res = sim.get_result()
-            info(f'Simulated results = {sim_res}')
 
             simulator_results.append(sim_res)
         simulation_results['simulator_results'] = simulator_results
 
-        confidence_intervals = {
-            alpha: t.interval(alpha=alpha, df=len(simulator_results) - 1,
-                              loc=mean(simulator_results),
-                              scale=sem(simulator_results))
-            for alpha in [0.95, 0.99]
-        }
-
-        simulation_results['confidence_intervals'] = confidence_intervals
+        # confidence_intervals = {
+        #     alpha: t.interval(alpha=alpha, df=len(simulator_results) - 1,
+        #                       loc=mean(simulator_results),
+        #                       scale=sem(simulator_results))
+        #     for alpha in [0.95, 0.99]
+        # }
+        #
+        # simulation_results['confidence_intervals'] = confidence_intervals
         return simulation_results
 
     def get_rng(self):
@@ -100,37 +89,16 @@ class Simulation:
     def get_results(self):
         return self.results
 
-    def show_results(self):
-        results = self.results
-        if not results:
-            results = self.load_json(self.results_path)
-
-        # fix_servers_fix_prob = [
-        #     result for result in results if
-        #     result['servers'] == 9 and result['target_probability'] == 0.2
-        # ]
-        #
-        # fix_servers_fix_mi = [
-        #     result for result in results if
-        #     result['servers'] == 9 and result['mi'] == 0.4
-        # ]
-        #
-        # fix_prob_fix_mi = [
-        #     result for result in results if
-        #     result['target_probability'] == 0.2 and result['mi'] == 1
-        # ]
-
 
 def main():
     """Testing simulation."""
 
-    logger = setup_logger(level=DEBUG)
+    logger = setup_logger()
 
     logger.info('Starting simulation')
 
     sim = Simulation('config/config.json', 'results.json')
     sim.run()
-    sim.show_results()
 
 
 if __name__ == '__main__':
--- simulation/simulator.py
+++ simulation/simulator.py
@@ -15,69 +15,67 @@ class Simulator:
         self.events_limit = events_limit  # Max number of events
         self.busy = 0  # Busy servers counter
         self.start_time = 0  # Simulation start time
-        self.arrivals = 0  # Incoming packet counter
+        self.arrivals = 0  # Incoming clients counter
         self.queued = 0  # Queue length counter
-        self.served = 0  # Served packet counter
+        self.served = 0  # Served clients counter
         self.event_list: List[Tuple] = []  # Event list
+        self.history_list: List[Tuple] = []  # Historical event list
         self.rng = default_rng(seed)  # Random number generator
-        self.blocking_probability = 0
+        self.results = {}  # Dict with statistics for each event
 
     def end(self):
         """End simulation condition."""
 
         return ((time() - self.start_time) > self.time_limit) or \
-               (self.arrivals >= self.events_limit)
+               (self.served >= self.events_limit)
 
     def run(self):
         """Run simulation."""
 
-        info('Starting simulation')
+        debug('Starting simulation')
         self.start_time = time()
 
         # Add primary event on list
-        ev = ('arrival', self.start_time)
-        self.event_list.append(ev)
         self.arrivals += 1
+        ev = ('arrival', self.start_time, self.arrivals)
+        self.event_list.append(ev)
         debug(f'Adding to event list {ev}')
 
         while not self.end():
             # Take event from list
             ev = self.pop_list()
-            ev_type, ev_time = ev
+            ev_type, ev_time, ev_id = ev
             debug(f'New event appeared {ev}')
 
             if ev_type == 'arrival':
                 if not self.servers_busy():
-                    eos_ev = (f'end_of_service_{self.busy + 1}',
-                              ev_time + self.serve_time())
-                    self.event_list.append(eos_ev)
                     self.busy += 1
+                    eos_ev = (f'end_of_service', ev_time + self.serve_time(),
+                              ev_id)
+                    self.event_list.append(eos_ev)
                     debug(f'Adding to event list {eos_ev}')
                 else:
-                    # TODO: SPRAWDZIC CZY SIE NIE JEBIE TU PRZY UZYCIU
-                    #  last_end_of_service_time(), latwiej bedzie bez
-                    #  multiprocessingu i poola...
-                    wait_ev = (f'waiting_{self.queued + 1}',
-                               self.earliest_eos_time())
-                    self.event_list.append(wait_ev)
                     self.queued += 1
+                    wait_ev = (f'waiting', self.earliest_eos_time(), ev_id)
+                    self.event_list.append(wait_ev)
                     debug(f'Adding to event list {wait_ev}')
 
-                new_ev = ('arrival', ev_time + self.arrival_time())
-                self.event_list.append(new_ev)
                 self.arrivals += 1
+                new_ev = ('arrival', ev_time + self.arrival_time(),
+                          self.arrivals)
+                self.event_list.append(new_ev)
                 debug(f'Adding to event list {new_ev}')
 
             elif 'waiting' in ev_type:
                 if not self.servers_busy():
-                    eos_ev = (f'end_of_service_{self.busy + 1}',
-                              ev_time + self.serve_time())
-                    self.event_list.append(eos_ev)
                     self.busy += 1
                     self.queued -= 1
+                    eos_ev = (f'end_of_service', ev_time + self.serve_time(),
+                              ev_id)
+                    self.event_list.append(eos_ev)
                     debug(f'Adding to event list {eos_ev}')
                 else:
-                    wait_ev = (f'{ev_type}', self.earliest_eos_time())
+                    wait_ev = (f'{ev_type}', self.earliest_eos_time(), ev_id)
                     self.event_list.append(wait_ev)
                     debug(f'Updating in event list {wait_ev}')
 
@@ -86,14 +84,7 @@ class Simulator:
                 self.busy -= 1
                 debug(f'{ev_type}: Incrementing served, decrementing busy')
 
-        # TODO: statystyki z githuba podjebac KAROLINAAAAA
-        self.blocking_probability = self.queued / self.arrivals
-
-        info(f'Results: '
-             f'arrivals = {self.arrivals}, '
-             f'served = {self.served}, '
-             f'queued = {self.queued}, '
-             f'P_block = {self.blocking_probability}')
+        debug('Simulation done')
 
     def pop_list(self):
         """Returns next to come event."""
@@ -104,6 +95,13 @@ class Simulator:
         ev = self.event_list[0]
         # Remove it from list by overwriting list bypassing zero element
         self.event_list = self.event_list[1:]
+
+        # Statistics update
+        if self.results.get(ev[2], False):
+            self.results[ev[2]].append(ev)
+        else:
+            self.results[ev[2]] = [ev]
+
         # Return the event
         return ev
 
@@ -133,9 +131,10 @@ class Simulator:
 
     def servers_busy(self):
         """Check if server is busy by comparing number of end_of_service
-        events with number of servers. """
+        events with number of servers."""
 
         return self.busy == self.servers
 
     def get_result(self):
-        return self.blocking_probability
+        # TODO: zrobic zeby dalo sie liczyc przedzialy ufnosci i srednia z K symulacji roznych parametrow
+        return self.history_list
